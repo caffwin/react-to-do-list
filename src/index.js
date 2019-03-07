c34c5874ff2c41e866f1fe49ca95848ae7baaa2e
caffwin <catheralligator@gmail.com>
2019-03-06 23:58:04 -0800

    converting functional components into class-based

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 // import App from "./App";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Greeting />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="react_practice" />
+        <Greeting />
+      </div>
+    );
+  }
 }
 
-function Header(props) {
-  return (
-    <header>
-      <p>Welcome!, {props.username}</p>
-    </header>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}!</p>
+      </header>
+    );
+  }
 }
 
-function Greeting() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours < 12) {
+      timeOfDay = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon";
+    } else {
+      timeOfDay = "night";
+    }
+    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
