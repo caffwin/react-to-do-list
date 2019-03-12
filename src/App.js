ea03653fcd416b521446171124a1dbd9fca6c6f8
caffwin <catheralligator@gmail.com>
2019-03-11 17:06:41 -0700

    changed + id displayed for handleChange method

--- src/App.js
+++ src/App.js
@@ -2,50 +2,55 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todoData from "./todoData";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        count: prevState.count + 1 // return new version of state
-      };
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Change</button>
-        {/* Can take obj literal or function that takes prev state as param, returning object literal that represents new version of state */}
-        {/* <ChildComponent count={this.state.count} /> */}
-      </div>
-    );
-  }
-}
-
 // class App extends React.Component {
 //   constructor() {
 //     super();
 //     this.state = {
-//       todos: todoData
+//       count: 0
 //     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.setState(prevState => {
+//       return {
+//         count: prevState.count + 1 // return new version of state
+//       };
+//     });
 //   }
 
 //   render() {
-//     const todoItems = this.state.todos.map(item => (
-//       <TodoItem key={item.id} item={item} />
-//     ));
-
-//     return <div>{todoItems}</div>;
+//     return (
+//       <div>
+//         <h1>{this.state.count}</h1>
+//         <button onClick={this.handleClick}>Change</button>
+//         {/* Can take obj literal or function that takes prev state as param, returning object literal that represents new version of state */}
+//         {/* <ChildComponent count={this.state.count} /> */}
+//       </div>
+//     );
 //   }
 // }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todoData
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    console.log("Changed", id);
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div>{todoItems}</div>;
+  }
+}
 
 export default App;
